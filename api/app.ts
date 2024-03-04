aa93c871e9aad90ba5af6d8259e86dfb56b5f587
OliFarhaan <alifarhan231087@gmail.com>
2024-03-05 04:36:28 +0530

    Some optimization done

--- api/app.ts
+++ api/app.ts
@@ -20,7 +20,7 @@ const corsOptions = {
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(cors(corsOptions));
-// app.use(verifyToken);
+app.use(verifyToken);
 
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({
